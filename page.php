0b49869858434bf2a08d3456e38e7fa5f384b257
Adam Pisula <adam.pisula@outlook.com>
2018-10-10 23:37:27 +0200

    Ads

--- page.php
+++ page.php
@@ -27,6 +27,15 @@
     $post = get_post($id); 
     $content = apply_filters('the_content', $post->post_content);
 
+    $paragraphs = explode("\n", $content);
+
+
+    for($i = 1; $i <= count($paragraphs) / 6 - 1; $i++) {
+        array_splice($paragraphs, $i * 6 + ($i - 1), 0, array("<div class='ad ad_content'></div>"));
+    }
+
+    $content = implode('<br />', $paragraphs);
+
 ?>
 
 <body>
@@ -83,6 +92,83 @@
         <p class="title"><?php echo get_the_title(); ?></p>
         <span class="content"><?php echo $content; ?></span>
     </div>
+
+    <script>
+        //ADS
+        $(window).on("resize load", function() {
+            if($(".container").width() >= 740) {
+                $(".ad_content").removeClass("ad_largerectangle");
+                $(".ad_content").removeClass("ad_rectangle");
+                $(".ad_content").addClass("ad_popunder");
+            }
+
+            else if ($(".container").width() >= 380) {
+                $(".ad_content").removeClass("ad_popunder");
+                $(".ad_content").removeClass("ad_rectangle");
+                $(".ad_content").addClass("ad_largerectangle");
+            }
+
+            else {
+                $(".ad_content").removeClass("ad_popunder");
+                $(".ad_content").removeClass("ad_largerectangle");
+                $(".ad_content").addClass("ad_rectangle");
+            }
+
+            if($(".container").offset().left >= 280) {
+                if($(window).height() >= 440) {
+                    if(!$(".ad_side").hasClass("ad_verticalrectangle")) {
+                        console.log("ad_verticalrectangle");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_verticalrectangle'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_verticalrectangle'></div>");
+                    }
+                }
+            }
+
+            else if($(".container").offset().left >= 200) {
+                if($(window).height() >= 640) {
+                    if(!$(".ad_side").hasClass("ad_wideskyscraper")) {
+                        console.log("ad_wideskyscraper");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_wideskyscraper'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_wideskyscraper'></div>");
+                    }
+                }
+            }
+
+            else if($(".container").offset().left >= 160) {
+                if($(window).height() >= 640) {
+                    if(!$(".ad_side").hasClass("ad_skyscraper")) {
+                        console.log("ad_skyscraper");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_skyscraper'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_skyscraper'></div>");
+                    }
+                }
+
+                else if($(window).height() >= 280) {
+                    if(!$(".ad_side").hasClass("ad_verticalbanner")) {
+                        console.log("ad_verticalbanner");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_verticalbanner'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_verticalbanner'></div>");
+                    }
+                }
+            }
+
+            else
+                $(".ad_side").remove();
+
+            var d = ($(".container").offset().left - $(".ad_side").width()) / 2;
+
+            $(".ad_left").css("left", d);
+            $(".ad_right").css("right", d);
+        });
+    </script>
     <div class="scroll_up">↑</div>
     <div class="menu_narrow"><?php wp_nav_menu(array("depth" => 1)); ?></div>
 </body>
--- single.php
+++ single.php
@@ -85,6 +85,65 @@
         </span>
         <p class="content"><?php echo $post->post_content; ?></p>
     </div>
+
+    <script>
+        //ADS
+        $(window).on("resize load", function() {
+            if($(".container").offset().left >= 280) {
+                if($(window).height() >= 440) {
+                    if(!$(".ad_side").hasClass("ad_verticalrectangle")) {
+                        console.log("ad_verticalrectangle");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_verticalrectangle'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_verticalrectangle'></div>");
+                    }
+                }
+            }
+
+            else if($(".container").offset().left >= 200) {
+                if($(window).height() >= 640) {
+                    if(!$(".ad_side").hasClass("ad_wideskyscraper")) {
+                        console.log("ad_wideskyscraper");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_wideskyscraper'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_wideskyscraper'></div>");
+                    }
+                }
+            }
+
+            else if($(".container").offset().left >= 160) {
+                if($(window).height() >= 640) {
+                    if(!$(".ad_side").hasClass("ad_skyscraper")) {
+                        console.log("ad_skyscraper");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_skyscraper'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_skyscraper'></div>");
+                    }
+                }
+
+                else if($(window).height() >= 280) {
+                    if(!$(".ad_side").hasClass("ad_verticalbanner")) {
+                        console.log("ad_verticalbanner");
+
+                        $(".ad_side").remove();
+                        $("body").append("<div class='ad ad_side ad_left ad_verticalbanner'></div>");
+                        $("body").append("<div class='ad ad_side ad_right ad_verticalbanner'></div>");
+                    }
+                }
+            }
+
+            else
+                $(".ad_side").remove();
+
+            var d = ($(".container").offset().left - $(".ad_side").width()) / 2;
+
+            $(".ad_left").css("left", d);
+            $(".ad_right").css("right", d);
+        });
+    </script>
     <div class="scroll_up">↑</div>
     <div class="menu_narrow"><?php wp_nav_menu(array("depth" => 1)); ?></div>
 </body>
