8be08fe4b4da5bdfbe82913bcf4d66bfafc6f5ea
Adam Pisula <adam.pisula@outlook.com>
2018-12-08 10:06:55 +0100

    Fix #2

--- add-photo.php
+++ add-photo.php
@@ -1,3 +1,21 @@
+<?php if ( !function_exists('is_user_logged_in') ) :
+/**
+* Checks if the current visitor is a logged in user.
+*
+* @since 2.0.0
+*
+* @return bool True if user is logged in, false if not logged in.
+*/
+function is_user_logged_in() {
+$user = wp_get_current_user();
+
+if ( empty( $user->ID ) )
+return false;
+
+return true;
+}
+endif;
+?>
 <?php   if(is_user_logged_in()): ?>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <p>Image:</p>
